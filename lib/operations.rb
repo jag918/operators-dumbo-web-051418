8cab3a1b06f023e2254dbd1973acea90a2820261
jee <jag918@nyu.edu>
2018-05-04 12:59:36 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,16 @@
 def unsafe?(speed)
   if speed <40 || speed>60
-    "true"
+    true
   else
-    "false"
+    false
   end
 end
 
 
 
 def not_safe?(speed)
-  speed>60? "true":"false"
-  speed<40? "true":"false"
+  speed>60? true:false
+  speed<40? true:false
 	
 end
 	
